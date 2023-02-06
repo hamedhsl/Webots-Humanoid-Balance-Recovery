fc91cfcff9209f81a3e0a2a1d9a682dad52a4329
Soheil Khatibi <soheilkhatibi1377@gmail.com>
2023-02-06 23:28:13 +0330

    Add world and controller files.

---
+++ controllers/balanceRecoveryController/balanceRecoveryController.py
@@ -0,0 +1,15 @@
+from controller import Robot, Motor
+from math import pi, sin
+
+TIME_STEP = 32
+
+robot = Robot()
+motor = robot.getDevice("my_motor")
+
+F = 2.0   # frequency 2 Hz
+t = 0.0   # elapsed simulation time
+
+while robot.step(TIME_STEP) != -1:
+    position = sin(t * 2.0 * pi * F)
+    motor.setPosition(position)
+    t += TIME_STEP / 1000.0
